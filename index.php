d932bdaed74c164b2467586b08f433e4cff9d13e
giuseppesciacca <sciaccagi@gmail.com>
2023-05-16 16:11:48 +0200

    [add] class Movie and Genre, + add constructor

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
 <?php
+
+/* classe ‘Movie’
+all'interno della classe sono dichiarate delle variabili d'istanza
+all'interno della classe è definito un costruttore
+all'interno della classe è definito almeno un metodo */
+class Movie
+{
+    public $title;
+    public $overview;
+    public $genre;
+
+    function __construct(string $title, string $overview, Genre $genre)
+    {
+        $this->title = $title;
+        $this->overview = $overview;
+        $this->genre = $genre;
+    }
+}
+class Genre
+{
+    public $genre;
+
+    function __construct(string $genre)
+    {
+        $this->genre = $genre;
+    }
+}
+
+$avatar = new Movie('Avatar', 'Jake Sully è un marine costretto su una sedia a rotelle che accetta di trasferirsi sul pianeta Pandora in sostituzione del fratello morto, la cui missione era quella di esplorare il pianeta. Essendo l\'atmosfera del pianeta tossica per gli umani sono stati creati degli esseri simili ai nativi Na\'vi che possono essere \'guidati\' dagli umani.', new Genre('Action'));
+
+var_dump($avatar);
+
+$titanic = new Movie('Avatar', 'A Clerville c\'è attesa per l\'arrivo di Lady Kant, che porta con sé un diamante rosa. Il gioiello non sfugge all\'attenzione di Diabolik, che rimane incantato dalla donna. Ma l\'ispettore Ginko ha trovato il modo di intrappolare il criminale.', new Genre('Crime'));
+
+var_dump($titanic);
